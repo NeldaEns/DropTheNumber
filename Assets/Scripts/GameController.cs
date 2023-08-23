151cec0558b744c4ad477e5f7092c71c6fee890b
NeldaEns <sang82168@gmail.com>
2023-08-23 11:05:45 +0700

    fix

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,6 +8,10 @@ public class GameController : MonoBehaviour
     public static GameController ins;
     public GameObject numberPrefab;
     public Transform tileContainer;
+    public Transform objectParent;
+    public int rows = 6;
+    public int columns = 5;
+    private Box[,] tiles;
 
     private void Awake()
     {
@@ -24,6 +28,7 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
+        InitializeTiles();
         SpawnNumber();
         SpawnNumber();
     }
@@ -51,8 +56,90 @@ public class GameController : MonoBehaviour
         }
     }
 
+    void InitializeTiles()
+    {
+        tiles = new Box[rows, columns];
+
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < columns; j++)
+            {
+                GameObject newTileGO = Instantiate(numberPrefab, tileContainer);
+                Box newTile = newTileGO.GetComponent<Box>();
+                tiles[i, j] = newTile;
+            }
+        }
+    }
+
     public void MoveBox(Vector2 direction)
     {
+        bool tilesMovedOrMerged = false;
+
+        Vector2Int step = Vector2Int.zero;
+        if (direction == Vector2.up) step = Vector2Int.up;
+        else if (direction == Vector2.down) step = Vector2Int.down;
+        else if (direction == Vector2.left) step = Vector2Int.left;
+        else if (direction == Vector2.right) step = Vector2Int.right;
+
+        for (int i = 0; i < rows; i++)
+        {
+            List<Box> tilesInLine = new List<Box>();
+
+            for (int j = 0; j < columns; j++)
+            {
+                int rowIndex = (step == Vector2Int.up || step == Vector2Int.down) ? j : i;
+                int columnIndex = (step == Vector2Int.left || step == Vector2Int.right) ? j : i;
+                Box tile = tiles[rowIndex, columnIndex];
+
+                if (tile != null)
+                {
+                    tilesInLine.Add(tile);
+                }
+            }
+
+            List<Box> mergedTiles = new List<Box>();
+            for (int j = 0; j < tilesInLine.Count; j++)
+            {
+                if (tilesInLine[j] == null)
+                    continue;
+
+                int targetIndex = j;
+                while (targetIndex - 1 >= 0 && tilesInLine[targetIndex - 1] == null)
+                {
+                    targetIndex--;
+                }
+
+                if (targetIndex != j)
+                {
+                    tilesInLine[targetIndex] = tilesInLine[j];
+                    tilesInLine[j] = null;
+                    tilesMovedOrMerged = true;
+                }
+
+                if (targetIndex - 1 >= 0 && tilesInLine[targetIndex - 1].value == tilesInLine[targetIndex].value &&
+                    !mergedTiles.Contains(tilesInLine[targetIndex - 1]))
+                {
+                    tilesInLine[targetIndex - 1].value *= 2;
+                    mergedTiles.Add(tilesInLine[targetIndex - 1]);
+                    tilesInLine[targetIndex] = null;
+                    tilesMovedOrMerged = true;
+                }
+            }
+
+            for (int j = 0; j < tilesInLine.Count; j++)
+            {
+                if (tilesInLine[j] != null)
+                {
+                    int rowIndex = (step == Vector2Int.up || step == Vector2Int.down) ? j : i;
+                    int columnIndex = (step == Vector2Int.left || step == Vector2Int.right) ? j : i;
+                    tiles[rowIndex, columnIndex] = tilesInLine[j];
+                }
+            }
+        }
+
+        if (tilesMovedOrMerged)
+        {
+        }
     }
 
 
@@ -62,5 +149,6 @@ public class GameController : MonoBehaviour
         GameObject newTile = Instantiate(numberPrefab, tileContainer);
         int newValue = Random.Range(1, 3) * 2;
         newTile.GetComponent<Box>().Initialize(newValue);
+        newTile.transform.SetParent(objectParent.transform);
     }
 }
---
+++ Assets/Scripts/ObjectParent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectParent : MonoBehaviour
+{
+    public Collider2D coll;
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+}
